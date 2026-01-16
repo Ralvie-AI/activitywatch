69287b57e4c0cc6173c818b830678a29b1becc52
chompu <chompu@zktecodev.com>
2026-01-16 10:12:58 +0700

    delete sd-pixel-engine folder

--- sd-pixel-engine/sd_pixel_engine/test.py
+++
@@ -1,8 +0,0 @@
-from mss import mss
-from PIL import Image
- 
-with mss() as sct:
-    monitor = sct.monitors[0]  # all monitors combined
-    screenshot = sct.grab(monitor)
-    img = Image.frombytes("RGB", screenshot.size, screenshot.rgb)
-    img.save("all_monitors3.png")
